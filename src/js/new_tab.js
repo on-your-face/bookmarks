a587077b3b9c7e99a50e99bedd64d690cc25212d
nexeption <nexeption@gmail.com>
2026-05-27 18:47:43 +0300

    Updates

--- src/js/new_tab.js
+++
@@ -1,6 +0,0 @@
-export function initNewTab() {
-   document.querySelectorAll('a.new_tab').forEach((el) => {
-      el.setAttribute('target', '_blank');
-      el.setAttribute('rel', 'noopener noreferrer');
-   });
-}
